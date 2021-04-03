93cea9a0cec6495eafba53435cc446b1ce4f2bc8
ympubrau <gvv55221133@gmail.com>
2021-04-03 10:46:29 +0300

    Small fixes

--- src/matrix_test.java
+++ src/matrix_test.java
@@ -30,29 +30,17 @@ public class matrix_test {
     }
 
     public static void getResult(int result) {
-        switch (result){
-            case 0:
-                System.out.println("Система имеет единственное решение \n");
-                break;
-            case 1:
-                System.out.println("Решений бесконечно много \n");
-                break;
-            case 2:
-                System.out.println("Решений нет \n");
-                break;
-            case 3:
-                System.out.println("Система вырожденная \n");
-                break;
-            case 4:
-                System.out.println("Систему можно решать дальше \n");
-                break;
-
+        switch (result) {
+            case 0 -> System.out.println("Система имеет единственное решение \n");
+            case 1 -> System.out.println("Решений бесконечно много \n");
+            case 2 -> System.out.println("Решений нет \n");
+            case 3 -> System.out.println("Система вырожденная \n");
+            case 4 -> System.out.println("Систему можно решать дальше \n");
         }
     }
 
     public static void printMatrix(double [] matrix) {
-        for (int i = 0; i < matrix.length; i++)
-            System.out.printf("%15.6E", matrix[i]);
+        for (double v : matrix) System.out.printf("%15.6E", v);
         System.out.println();
     }
 }
